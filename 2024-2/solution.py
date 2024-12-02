f50d539d897c7df44df755b1b78358dc97337d62
Jan J. Serra <jan.jime.serra@gmail.com>
2024-12-02 12:50:01 +0100

    2024-2-2 (brutte-forced)

--- 2024-2/solution.py
+++ 2024-2/solution.py
@@ -2,30 +2,34 @@ first_res = 0
 
 with open('input.txt', 'r') as file:
   for line in file:
-    safe = True
-    asc = None
-    numbers = line.split()
-    for i in range(1, len(numbers)):
-      prev_num = int(numbers[i-1])
-      curr_num = int(numbers[i])
-      diff = abs(prev_num - curr_num)
-      
-      if not (diff >= 1 and diff <= 3):
-        safe = False
-        continue
-
-      if asc == None:
-        asc = curr_num > prev_num
-      else: 
-        if asc: 
-          if curr_num < prev_num:
-            safe = False
-            continue
-        else:
-          if curr_num > prev_num:
-            safe = False
-            continue
-    first_res += safe
+    line_numbers = line.split()
+    for exclude in range(len(line_numbers)):
+      safe = True
+      asc = None
+      numbers = line_numbers[:exclude] + line_numbers[exclude+1:]
+      for i in range(1, len(numbers)):
+        prev_num = int(numbers[i-1])
+        curr_num = int(numbers[i])
+        diff = abs(prev_num - curr_num)
+        
+        if not (diff >= 1 and diff <= 3):
+          safe = False
+          continue
+        if asc == None:
+          asc = curr_num > prev_num
+        else: 
+          if asc: 
+            if curr_num < prev_num:
+              safe = False
+              continue
+          else:
+            if curr_num > prev_num:
+              safe = False
+              continue
+      if safe:
+        print(line, exclude)
+        first_res += safe
+        break
 
 print(f'first result: {first_res}')
 
